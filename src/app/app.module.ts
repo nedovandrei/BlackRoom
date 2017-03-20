688115946745da0ef5c74d8235ca5c83eb94ab83
Hatebreeder <dee.snake@gmail.com>
2017-03-20 23:34:06 +0200

    implemented angular 2 router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from "./home/home.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "gallery", component: GalleryComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
